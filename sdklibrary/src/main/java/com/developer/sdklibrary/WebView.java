59a43362f5102b6c0a4bd702e2216f4fceab133d
Kasun Chinthaka <kasun@odoc.life>
2019-12-18 08:02:33 +0530

    first-commit

---
+++ sdklibrary/src/main/java/com/developer/sdklibrary/WebView.java
@@ -0,0 +1,13 @@
+package com.developer.sdklibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class WebView {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
